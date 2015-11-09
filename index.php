33aa3f48b5ea6764bce0bef31020622576e791a1
Quesomora <quesomora@gmail.com>
2015-11-09 08:30:47 -0600

    1
    .

---
+++ index.php
@@ -0,0 +1,20 @@
+<HTML>
+	<head>
+		<title>BanUAA</title>
+	</head>
+	<body>
+		<?php
+			//Conexion
+			$db_host = "";
+			$db_user = "";
+			$db_pass = "";
+			$da_name = "";
+			//$link = mysqli_connect($db_host, $db_user, $db_pass, $da_name);
+			
+		?>
+		<form method="post">
+			<p>User<input type="text" placeholder=User  name="user"></p>
+			<p>Password<input type="password" placeholder=Pass name="pass"></p>
+		</form>
+	</body>
+</HTML>
